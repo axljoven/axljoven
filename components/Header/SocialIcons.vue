ea7e19e530aea6ac96eaa90d5bfb2dbaa2da2d63
Axl Joven <axlmorenojoven@gmail.com>
2019-04-03 20:29:34 +0800

    Change menu and badge background to accent color when hovered

--- components/Header/SocialIcons.vue
+++ components/Header/SocialIcons.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="social-icons">
         <li v-for="(social, id) in socials" :key="id" class="social-icons-item">
-            <a :href="social.to" :title="social.title">
+            <a :href="social.to" :title="social.title" :target="social.target">
                 <span :class="social.icon"></span>
             </a>
         </li>
@@ -17,17 +17,20 @@ export default {
                 { 
                     to: "https://ph.linkedin.com/in/axl-joven-363572177", 
                     title: "LinkedIn", 
-                    icon: "icon ion-logo-linkedin" 
+                    icon: "icon ion-logo-linkedin" ,
+                    target: "_blank"
                 },
                 { 
                     to: "https://web.facebook.com/axl.joven", 
                     title: "Facebook", 
-                    icon: "icon ion-logo-facebook" 
+                    icon: "icon ion-logo-facebook" ,
+                    target: "_blank"
                 },
                 { 
                     to: "mailto:https://web.facebook.com/axl.joven", 
                     title: "Gmail", 
-                    icon: "icon ion-logo-google" 
+                    icon: "icon ion-logo-google" ,
+                    target: "_blank"
                 },
             ]
         }
